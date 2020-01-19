c53e774a5a920bfc63a2ce74089f6ed0d8b2ae09
meryemkmr <meryemkmr36@gmail.com>
2020-01-19 12:07:45 -0600

    homework

---
+++ 01-week/4-friday/homework.py
@@ -0,0 +1,48 @@
+# Small
+#1. Phonebook dictionary
+
+phonebook_dict = {
+'Alice': '703-493-1834',
+'Bob': '857-384-1234',
+'Elizabeth': '484-584-2923'
+}
+
+# myphone = phonebook_dict.get("Elizabeth")
+# print(myphone)
+
+# phonebook_dict["Kareem"] = "938-489-1234"
+# print(phonebook_dict)
+# del phonebook_dict["Alice"]
+# print(phonebook_dict)
+# phonebook_dict["Bob"] = "968-345-2345"
+# print(phonebook_dict)
+
+
+# 2. Nested dictionaries
+
+# ramit = {
+# 'name': 'Ramit',
+# 'email': 'ramit@gmail.com',
+# 'interests': ['movies', 'tennis'],
+# 'friends': [
+#     {
+#     'name': 'Jasmine',
+#     'email': 'jasmine@yahoo.com',
+#     'interests': ['photography', 'tennis']
+#     },
+#     {
+#     'name': 'Jan',
+#     'email': 'jan@hotmail.com',
+#     'interests': ['movies', 'tv']
+#     }
+# ]
+# }
+
+my_dictionary = {
+    "hello" :   "world",
+    "sqareOfTwo" : 4,
+    "theMeaningOfLife" : 42,
+    0 : 1
+}
+helloIs = my_dictionary["hello"]
+print(helloIs)
