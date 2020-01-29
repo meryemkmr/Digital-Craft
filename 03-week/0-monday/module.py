ba22f19a14fa1b0ed0ebb19de471e36dfcfad8a5
meryemkmr <meryemkmr36@gmail.com>
2020-01-29 14:13:01 -0600

    position

---
+++ 03-week/0-monday/module.py
@@ -0,0 +1,16 @@
+# from random import choice
+# places = ["McDonalds", "KFC", "Burger King", "Taco Bell","Wendys", "Arbys", "Pizza Hut"]
+# def pick():  # see the docstring below?
+#     """Return random fast food place"""
+#     return choice(places)
+
+
+# print(pick())
+# print(pick())
+
+# from random import randint
+
+# if randint(1,5) == 2:
+#     print("2 is  the number")
+# else:
+#     print("another number")
