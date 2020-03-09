f3f205549410f2d7e2c91e5eb24252f3342de92d
meryemkmr <meryemkmr36@gmail.com>
2020-03-09 13:19:54 -0500

    updated

---
+++ 09-week/0-monday/lectures/firstDB.sql
@@ -0,0 +1,27 @@
+-- CREATE DATABASE secondDB;
+
+-- CREATE TABLE student(
+   
+--     name varchar,
+--     website varchar,
+--     github_username varchar,
+--     points integer,
+--     gender char(1),
+--     cohort_start_date date,
+--     graduated boolean
+-- );
+
+-- CREATE TABLE student2(
+--    id SERIAL NOT NULL PRIMARY KEY,
+--     name varchar,
+--     website varchar,
+--     github_username varchar,
+--     points integer,
+--     gender char(1),
+--     cohort_start_date date,
+--     graduated boolean
+-- );
+
+INSERT INTO student2 VALUES
+ (DEFAULT, 'Meryem', 'DifgitalCrafts.com','meryemkmr',5,'F','2020-03-09',FALSE),
+ (DEFAULT, 'Kazim', 'DifgitalCrafts.com','meryemkmr',5,'M','2020-03-09',FALSE);
