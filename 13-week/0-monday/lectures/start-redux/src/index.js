dac27c1b2804ae1a555ae6835d93706d790a730a
Meryem Kömür <meryemkmr36@gmail.com>
2020-04-12 21:27:40 -0500

    last cahnge

--- 13-week/0-monday/lectures/start-redux/src/index.js
+++ 13-week/0-monday/lectures/start-redux/src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+// import App from "./App";
+import React, { Component } from "react";
 
+
+
+//Goal is to increment the global state
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "hello"
+    };
+  }
+  render() {
+    return (
+      <>
+        <h1>{this.state.title}</h1>
+        <span>{this.props.count}</span>
+        <button onClick={this.props.onIncreaseClick}>Increase</button>
+      </>
+    );
+  }
+}
+//Maps global state to property in component
+let mapStateToProps = state => {
+  return {
+    count: state.count
+  };
+};
+//Allows the store to invoke the function. Reason for syntax.
+let mapDispatchToProps = dispatch => {
+  return {
+    onIncreaseClick: () => dispatch(increaseAction())
+  };
+};
+const App = connect(mapStateToProps, mapDispatchToProps)(Counter);
+let increaseAction = () => {
+  return {
+    type: "Increase"
+  };
+};
+let initialState = {
+  count: 0
+};
+let counterReducer = (state, action) => {
+  if (state === undefinded) {
+    state = initialState;
+  }
+  if (action.type === "Increase") {
+    return {
+      ...state,
+      count: state.count + 1
+    };
+  } else {
+    return state;
+  }
+};
+let store = createStore(counterReducer);
+// export default Counter;
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
